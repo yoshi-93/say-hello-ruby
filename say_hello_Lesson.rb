e9d39ed61ee03423388333e387f79ea3c2234e80
Mario Lopez <ez0212sp@icloud.com>
2020-12-09 18:18:48 -0500

    commit tree 7a213bab0c8baf4826591672882a5a8dee3f98ff

---
+++ say_hello_Lesson.rb
@@ -0,0 +1,30 @@
+## Build your say_hello method here
+#
+#def greeting(name)
+#    puts "Hello, #{name}."
+#  end 
+#  greeting("Buklau")
+
+#             assigning a default value
+#def greeting(name = "Ruby programmer")
+#    puts "Hello, #{name}"
+#  end 
+#  #greeting
+#  greeting("Sophie")
+
+#def greeting(name="Ruby programmer", language="Ruby")
+#    puts "Hello, #{name}. We heard you are a great #{language} programmer."
+#  end 
+#  greeting("Steven","Elixir")
+
+#def greeting(name, language="Ruby")
+#    puts "Hello, #{name}. We heard you are a great #{language} programmer."
+#  end 
+#  greeting("Sophie","Ember.js")
+#  greeting("Dan")
+
+#def greeting(language="Ruby", name)
+#    puts "Hello, #{name}. We heard you are a great #{language} programmer."
+#  end 
+#  greeting("Sophie")
+  
